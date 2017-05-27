aa981040a9b0d57e20f2e68b52eefb0fc4cccee9
Jay Backof <jaybackof@wlccorp.com>
2017-05-27 22:37:02 +0000

    mocha tests

---
+++ test/test.js
@@ -0,0 +1,29 @@
+var test = require('unit.js');
+var str = 'Hello, world!';
+
+describe('CRUD Test', function() {
+  it('create', function() {
+    
+        test.string('test').contains(
+            'test');
+  });
+  
+  it('read', function() {
+    
+        test.string('test').contains(
+            'test');
+  });
+  
+   it('upadte', function() {
+    
+        test.string('test').contains(
+            'test');
+  });
+  
+   it('delete', function() {
+    
+        test.string('test').contains(
+            'test');
+  });
+  
+});
